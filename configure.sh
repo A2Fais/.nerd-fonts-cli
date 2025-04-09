8995477dc42383220028fdd969dcca14ef1f2c5d
A2Fyise <aseeralfaisal@gmail.com>
2025-04-09 22:04:32 +0600

    removed . from .install.sh

--- configure.sh
+++ configure.sh
@@ -5,7 +5,7 @@ configure_shell() {
   local config='
 nerd_fonts_cli() {
   cd ~/.nerd-fonts-cli/ || return
-  chmod +x .install.sh
+  chmod +x install.sh
   ./install.sh
 }
 alias nerd-fonts="nerd_fonts_cli"
